2ce0e8db8c664a074f5188cc35dfd6c5a2a97280
J2N8 <111210732+J2N8@users.noreply.github.com>
2022-11-17 21:38:23 -0500

    Updated Footer and Added animated icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import SearchContainer from "./SearchContainer";
-import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -8,9 +7,36 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <SearchContainer defaultCity="Tampa" />
-      <br />
-      <Footer />
+        <SearchContainer defaultCity="Tampa" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/jasmine-poole-421b101b7/"
+            target="_blank"
+            className="linkedIn"
+            rel="noopener noreferrer"
+          >
+            Jasmine Poole
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/J2N8/weather-react"
+            target="_blank"
+            className="github"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          <a
+            href="https://weatherapp2-reactjs.netlify.app/"
+            target="_blank"
+            className="netlify"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/WeatherContainer.js
+++ src/WeatherContainer.js
@@ -1,57 +1,53 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 import "./App.css";
 
 export default function WeatherContainer(props) {
-    return (
-      <div className="weatherContentContainer">
-        <div className="currentCityContainer">
-          <div className="Row">
-            <div className="cityWeather Col">
-              <span className="cityName" id="City">
-                {props.data.city}
+  return (
+    <div className="weatherContentContainer">
+      <div className="currentCityContainer">
+        <div className="Row">
+          <div className="cityWeather Col">
+            <span className="cityName" id="City">
+              {props.data.city}
+            </span>
+            <br />
+            <span className="currentTime" id="DateLine">
+              <FormattedDate date={props.data.date} />
+            </span>
+            <br />
+            <span className="weatherDescription" id="Description">
+              {props.data.description}
+            </span>
+            <br />
+            <div className="weatherTemperature">
+              <span className="currentTemp" id="mainTemp">
+                {Math.round(props.data.temperature)}
               </span>
-              <br />
-              <span className="currentTime" id="DateLine">
-                <FormattedDate date={props.data.date} />
-              </span>
-              <br />
-              <span className="weatherDescription" id="Description">
-                {props.data.description}
-              </span>
-              <br />
-              <div className="weatherTemperature">
-                <span className="currentTemp" id="mainTemp">
-                  {Math.round(props.data.temperature)}
-                </span>
-                <span className="Units">
-                </span>
-              </div>
-            </div>
-            <div className="currentWeatherIcon">
-              <img
-                src={props.data.iconUrl}
-                id="icon"
-                alt={props.data.description}
-              />
-            </div>
-            <div className="currentWeatherDetails">
-              <ul>
-                <div className="Humidity">
-                  <li className="">
-                    Humidity: <span id="Humidity">{props.data.humidity}</span>%
-                  </li>
-                </div>
-                <div className="cWind">
-                  <li>
-                    Wind: <span id="Wind">{props.data.wind}</span> km/h
-                  </li>
-                </div>
-              </ul>
+              <span className="Units"></span>
             </div>
           </div>
+          <div className="currentWeatherIcon" id="icon">
+            <WeatherIcon code={props.data.icon} alt={props.data.description} />
+          </div>
+          <div className="currentWeatherDetails">
+            <ul>
+              <div className="Humidity">
+                <li className="">
+                  Humidity: <span id="Humidity">{props.data.humidity}</span>%
+                </li>
+              </div>
+              <div className="cWind">
+                <li>
+                  Wind: <span id="Wind">{props.data.wind}</span> km/h
+                </li>
+              </div>
+            </ul>
+          </div>
         </div>
-        <div className="cityNextTempContainer" id="Forecast"></div>
       </div>
-    );
+      <div className="cityNextTempContainer" id="Forecast"></div>
+    </div>
+  );
 }
